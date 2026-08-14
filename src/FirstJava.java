146f87c8bd780904793dffa7b1e0e7e3d19029a5
Andrei Basilio <andreibasilio4090@gmail.com>
2026-08-14 21:46:25 +0800

    First Java Program

---
+++ src/FirstJava.java
@@ -0,0 +1,10 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        //This will be my first Java program introducing myself via print statement :).
+        System.out.print("Hello, My name is Andrei Kyle I. Basilio."); /* Using print instead of println which means that the output after this will be
+        printed besides this line instead of a new line.*/
+        System.out.print(" I'm 19 years old."); // This output will be printed besides on the previous output.
+        System.out.println(" I was born and lived in Baguio City, Philippines."); /* The same applies here, it will be printed on the same
+        line because of the print statement from before this. */
+    }
+}
