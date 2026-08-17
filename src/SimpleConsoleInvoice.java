d52dc1b483716dd2570d0c4473f22afae308f7c8
Andrei Basilio <andreibasilio4090@gmail.com>
2026-08-17 14:26:23 +0800

    Added a new comment about myself.

--- src/SimpleConsoleInvoice.java
+++ src/SimpleConsoleInvoice.java
@@ -1,11 +1,16 @@
+// Basilio, Andrei Kyle I.
+// 1st year BSIT majoring in Web Technology
+// August 15, 2026
+// Project No. 2
+
 public class SimpleConsoleInvoice {
     public static void main(String[] args) {
-        //A Very simple program applying the usage of \t, \n, \", System.out.print, and System.out.println.
+        // A very simple program applying the usage of \t, \n, \", System.out.print, and System.out.println.
         System.out.println("Welcome to Simple Console Store\n");
-        System.out.println("\"Andrei's Sari-Sari Store\""); //My fictional sari-sari store
-        System.out.print("No. 63 Purok 64, Magsaysay Avenue, Baguio City, Philippines. 2600\n\n"); //Fictional address. Utilizing \n feature to break.
-        System.out.println("1.Downy Antibac 3packs 100g\t\t\t3x\t\tPHP 20"); //Hardcoded print statements for each product with their quantity and price.
-        System.out.println("2.Rebisco 10packs 60g\t\t\t\t2x\t\tPHP 70"); //Utilizing \t in formatting the output.
+        System.out.println("\"Andrei's Sari-Sari Store\""); // My fictional sari-sari store
+        System.out.print("No. 63 Purok 64, Magsaysay Avenue, Baguio City, Philippines. 2600\n\n"); // Fictional address. Utilizing \n feature to break.
+        System.out.println("1.Downy Antibac 3packs 100g\t\t\t3x\t\tPHP 20"); // Hardcoded print statements for each product with their quantity and price.
+        System.out.println("2.Rebisco 10packs 60g\t\t\t\t2x\t\tPHP 70"); // Utilizing the \t in formatting the output.
         System.out.println("3.Eggs\t\t\t\t\t\t\t\t15x\t\tPHP 120");
         System.out.println("4.Century Tuna Flakes & Oil 100g\t5x\t\tPHP 300");
         System.out.println("5.Purefoods Nuggets 250g\t\t\t2x\t\tPHP 180");
