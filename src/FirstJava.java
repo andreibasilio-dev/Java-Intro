c361dc72aa94177432826a92d706e8972b43b0f5
Andrei Basilio <andreibasilio4090@gmail.com>
2026-08-17 13:09:03 +0800

    Added a new comment about myself.

--- src/FirstJava.java
+++ src/FirstJava.java
@@ -1,8 +1,13 @@
+// Basilio, Andrei Kyle I.
+// 1st year BSIT majoring in Web Technology
+// August 15, 2026
+// Project No. 1
+
 public class FirstJava {
     public static void main(String[] args) {
         //This will be my first Java program introducing myself via print statement :).
-        System.out.print("Hello, My name is Andrei Kyle I. Basilio."); /* Using print instead of println that prints the output beside this line.*/
-        System.out.print(" I'm 19 years old."); // This print output will be printed besides on the previous print statement.
+        System.out.print("Hello, My name is Andrei Kyle I. Basilio."); // Using print instead of println to print on the same line.
+        System.out.print(" I'm 19 years old."); // This print on the previous line print this on the same line.
         System.out.println(" I was born and lived in Baguio City, Philippines."); /* The same applies here, it will be printed on the same
         line because of the print statement before this. */
     }
